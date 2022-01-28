3f67eeac3a403bad9a204817636141a1b817fa63
mmsura <89827589+mmsura@users.noreply.github.com>
2022-01-28 15:41:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-<script>
-  window.watsonAssistantChatOptions = {
-      integrationID: "7db7365c-9daf-4ad8-b95f-db7c7200f324", // The ID of this integration.
-      region: "eu-gb", // The region your integration is hosted in.
-      serviceInstanceID: "8f1c4d0a-c5d0-4283-9980-6a5a8afcd1a6", // The ID of your service instance.
-      onLoad: function(instance) { instance.render(); }
-    };
-  setTimeout(function(){
-    const t=document.createElement('script');
-    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js"
-    document.head.appendChild(t);
-  });
-</script>
+#templates are used to separate bussiness logic from presentation logic
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_view():
+	return render_template('index.html')
+	
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
